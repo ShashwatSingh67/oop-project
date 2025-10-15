ce80ae95e51851dd95e4fca3f77939feb9bd81ef
ShashwatSingh67 <a1970638@adelaide.edu.au>
2025-10-15 23:05:53 +1030

    setting up files for TradingBots class

---
+++ make_trading_decision.cpp
@@ -0,0 +1,2 @@
+//purpose of this class
+//
---
+++ trading_bots.cpp
@@ -0,0 +1,14 @@
+//purpose of this class is the manage the automated competing in the trading system
+//the attributes of members in the class will include type of trading strategy, risk tolerance, variance
+//aditional key methods; make_trading_decision hashmap
+//this is the main orchestra class for the rest of the trading bot functions
+//current key attributes:
+    //strategy_type: string
+    //risk_tolerance: double
+    //variance: double
+
+//key methods
+    //make_trading_decision: returns a hashmap<int, int> that states a desired stock postion for given stocks by id
+    //get trading strategy: return the strategy type
+    //double get variance
+    //void set variance
